386c0ad4eb70f4fdd5a1a22e90092657eeff48a9
Boldinov90 <detroid90@mail.ru>
2020-10-08 22:19:02 +0300

    s90

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
-// Настройки верхнего слайдера (JQUERY)
-$(document).ready(function(){
-    const headerSlider = $(".owl-carousel");
-    headerSlider.owlCarousel({
-        items: 1,
-        loop: true,
-        dots: false, 
-        smartSpeed: 2000
-    });
-});
 
-$('.customNextBtn').click(function() {
-    headerSlider.trigger('next.owl.carousel');
-})
-$('.customPrevBtn').click(function() {
-    headerSlider.trigger('prev.owl.carousel', [300]);
-})
+
+
+
 
