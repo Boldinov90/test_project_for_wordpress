e55e937aa830aaa35baabef9bb98021ca2c9efb0
Boldinov90 <detroid90@mail.ru>
2020-10-12 14:35:38 +0300

    s90

--- js/shop-slider.js
+++ js/shop-slider.js
@@ -1,16 +1,21 @@
 // Настройки нижнего слайдера (JQUERY)
 $(document).ready(function(){
     const shopSlider = $("#shopSlider");
-
     shopSlider.owlCarousel({
         items: 3,
         loop: true, // зацикливание
         dots: false, 
         margin: 2, 
-        smartSpeed: 500
+        smartSpeed: 500,
+        responsive:{
+            0: {
+                items:1
+            },
+            1254: {
+                items:3 
+            }
+        }
     });
-
-
 
     $('#shopSliderLeft').click(function() {
         shopSlider.trigger('prev.owl.carousel');
@@ -19,34 +24,4 @@ $(document).ready(function(){
     $('#shopSliderRight').click(function() {
         shopSlider.trigger('next.owl.carousel');
     });
-
-
-
-
-
-
-
-
-    // headerSlider.on('initialized.owl.carousel', function(event){
-    //     $('.slide-controls-number__active').text(event.item.index + 1);
-    //     $('.slide-controls-number__total').text(event.item.count);
-    // });
-
-    // headerSlider.owlCarousel({
-    //     items: 1,
-    //     // loop: true, // зацикливание
-    //     dots: false, 
-    //     smartSpeed: 2000
-    // });
-    // $('#headerSliderLeft').click(function() {
-    //     headerSlider.trigger('prev.owl.carousel');
-    // });
-    
-    // $('#headerSliderRight').click(function() {
-    //     headerSlider.trigger('next.owl.carousel');
-    // });
-    // headerSlider.on('changed.owl.carousel', function(event){
-    //     $('.slide-controls-number__active').text(event.item.index + 1);
-    //     $('.slide-controls-number__total').text(event.item.count);
-    // });
 });
